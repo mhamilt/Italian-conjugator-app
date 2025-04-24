7f5d313b6b79bb40e54bd9c26f59f7a3e3ad782f
knagle <keith@playable.tech>
2025-04-24 12:47:06 +0100

    Initial SwiftUI project

---
+++ italianConjugatorSwiftUI/italianConjugatorSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  italianConjugatorSwiftUI
+//
+//  Created by Keith Nagle on 24/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ italianConjugatorSwiftUI/italianConjugatorSwiftUI/italianConjugatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  italianConjugatorSwiftUIApp.swift
+//  italianConjugatorSwiftUI
+//
+//  Created by Keith Nagle on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct italianConjugatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
